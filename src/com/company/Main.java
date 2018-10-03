c8f9559f242de63cb056088008049232b98cd3a4
mynewtakenusername <mynewtakenusername@github.com>
2018-10-03 19:45:21 +0200

    homework02 completed

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,11 +2,15 @@ package com.company;
 
 public class Main {
     public static void main(String[] args) {
-        int age = 40;
-        boolean isVip = false;
-        System.out.println("discount = " + getDiscount(age, isVip));
-        System.out.println("discount = " + getDiscount(16, true));
-        System.out.println("discount = " + getDiscount(70, false));
+        //  int age = 40;
+       // boolean isVip = false;
+        //System.out.println("discount = " + getDiscount(age, isVip));
+        System.out.println("discount1 = " + getDiscount(40, false));
+        System.out.println("discount2 = " + getDiscount(40, true));
+        System.out.println("discount3 = " + getDiscount(65, false));
+        System.out.println("discount4 = " + getDiscount(65, true));
+        System.out.println("discount5 = " + getDiscount(16, false));
+        System.out.println("discount6 = " + getDiscount(16, true));
     }
 
     private static int getDiscount(int age, boolean isVip) {
